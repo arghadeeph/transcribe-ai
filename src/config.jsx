0b5de16a4379ea67478c58c51d8d8dfddf38d692
Arghadeep Halder <arghadeeph@gmail.com>
2026-03-25 14:06:46 +0530

    config chnages

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
-  // API_BASE_URL: "https://transcribe-ai-backend-6xfg.onrender.com",
-  API_BASE_URL: "http://127.0.0.1:8000",
+   API_BASE_URL: "https://transcribe-ai-backend-6xfg.onrender.com",
+  //API_BASE_URL: "http://127.0.0.1:8000",
 
 };
 
